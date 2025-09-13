446a2d6afabfffc0d063c49caa60cf7698af41ff
Rory Hackney <roryhackney@gmail.com>
2025-09-12 22:06:04 -0700

    update insert-content.php to remove old css classes

--- sql_drafting/insert-content.php
+++ sql_drafting/insert-content.php
@@ -63,7 +63,7 @@ function addList($items, $ordered = false) {
         }
         $html .= "</ol>";
     } else {
-        $html .= '<ul">';
+        $html .= '<ul>';
         foreach ($items as $item) {
             $html .= "<li>$item</li>";
         }
@@ -73,16 +73,43 @@ function addList($items, $ordered = false) {
 }
 
 function addStaticImage($src, $alt, $width, $height) {
-    addHTML('<img src="assets/' . $src . '" alt="' . $alt .'" width="' . $width . '" height="' . $height . '"/>');
+    addHTML('<img src="assets/posts/' . $src . '" alt="' . $alt .'" width="' . $width . '" height="' . $height . '"/>');
 }
 
 //MAKE CHANGES HERE
-$title = "EllePower Website - Capstone";
+$title = "Ecommerce SQL Database";
+
+addHTML('<p>For this project, I worked with <a href="https://github.com/KenCage1007\" target="_blank">Ken <span class="fas fa-up-right-from-square" title="opens in new tab"></span></a> to design and implement a relational SQL database for a fictional crystal shop called The Dragon\'s Hoard in order to develop our database skills. We wanted to track products, current inventory, transactions, reviews for products, and users who signed up for an account on the fictional shop\'s website.</p>');
+
+addContent("h2", null, "Features", null);
+
+addList([
+    "List which products we currently have in inventory",
+    "Create new products",
+    "Modify the amount of a particular product that we have in inventory",
+    "Delete a product from inventory",
+    "Get a list of the most popular products for a given time range",
+    "Get a list of the least popular products for a given time range",
+    "Get a list of users who haven't purchased something in a few months to send promotional emails to, this should also include products that these users normally purchase"
+]);
+
+addContent("h2", null, "Design", null);
+addHTML('<p>We began by analyzing and collecting requirements. As part of this process, we developed a <a href="https://github.com/roryhackney/ecommerceDatabase/blob/main/Glossary.pdf" target="_blank">glossary <span class="fas fa-up-right-from-square" title="opens in new tab"></span></a> which defined entities, naming conventions, and descriptions of various properties. We then developed an <a href="https://github.com/roryhackney/ecommerceDatabase/blob/main/design.png" target="_blank">entity relationship diagram (ERD) <span class="fas fa-up-right-from-square" title="opens in new tab"></span></a> using Crow\'s Foot notation to plan our logical model, defining each entity, its properties, and its relationships with other entities (eg, one to many). After normalization, we worked on the final <a href="https://github.com/roryhackney/ecommerceDatabase/blob/main/tableDiagram.png" target="_blank">table diagram <span class="fas fa-up-right-from-square" title="opens in new tab"></span></a> to define precisely how to implement each table in SQL, creating additional join tables for many to many relationships.</p>');
+
+addContent("h2", null, "Implementation", null);
+addText("Once the design was complete, we split up the tasks to write SQL to implement each table, allowing us to make progress on different independent tables at the same time. We also wrote SQL to insert data into each table. Finally, we developed SQL to create procedures and triggers. Once done, we combined our separate files, fixed bugs, and ensured everything worked together and successfully created the database when run in the correct order.");
+
+addStaticImage("ecommerce-sql-db-tables.png", "Screenshot of the database tables after implementation", "1482", "892");
+
+addContent("h2", null, "Application", null);
+addText("We then created a Java application which calls into the locally running database, using print statements for a menu and gathering user input in the terminal to demonstrate the functionality of the database.");
+
+addStaticImage("ecommerce-sql-db-application.png", "The Java program shows the menu: View Inventory, Add Product, Change Quantity, Delete Product, Get Most/Least Popular Products, Get Users Favorite Products For Promo", "1920", "1080");
 
 //Add GitHub and Hire Now buttons
-// $g = "https://github.com/roryhackney/job-offer-comparison-website";
-// $html = '<div class="buttons-row"><a class="button" href=' . $g . ' target="_blank">GitHub <span class="fas fa-up-right-from-square" title="opens in new tab"></span></a><a class="button" href="hire-me.php">Hire Now</a></div>';
-// addContent("html", null, $html, null);
+$g = "https://github.com/roryhackney/ecommerceDatabase";
+$html = '<div class="buttons-row"><a class="button" href=' . $g . ' target="_blank">GitHub <span class="fas fa-up-right-from-square" title="opens in new tab"></span></a><a class="button" href="hire-me.php">Hire Now</a></div>';
+addContent("html", null, $html, null);
 
 //Just add Hire Now button
 // addContent("html", null, '<div class="buttons-row"><a class="button urgent" href="hire-me.php">Hire Now</a></div>', null);
